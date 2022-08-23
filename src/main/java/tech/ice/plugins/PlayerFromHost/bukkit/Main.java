7a2e031cca64bf45a6744e238bbf42e59bc7bd82
YTiceice <a104321212@gmail.com>
2022-08-23 10:21:33 +0800

    feat: add bstats

--- src/main/java/tech/ice/plugins/PlayerFromHost/bukkit/Main.java
+++ src/main/java/tech/ice/plugins/PlayerFromHost/bukkit/Main.java
@@ -5,12 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 
+import org.bstats.bukkit.Metrics;
+
 public class Main extends JavaPlugin {
 
     public static Main BukkitPlayerFromHost;
 
     @Override
     public void onEnable() {
+        int ID = 16236;
+        Metrics metrics = new Metrics(this, ID);
+
         BukkitPlayerFromHost = this;
         Bukkit.getPluginManager().registerEvents(new Event(), this);
         Bukkit.getPluginCommand("pfhreload").setExecutor(new Reload());
--- src/main/java/tech/ice/plugins/PlayerFromHost/bungee/Main.java
+++ src/main/java/tech/ice/plugins/PlayerFromHost/bungee/Main.java
@@ -4,12 +4,17 @@ import net.md_5.bungee.api.plugin.Plugin;
 
 import java.io.IOException;
 
+import org.bstats.bungeecord.Metrics;
+
 public class Main extends Plugin {
 
     public static Main BungeePlayerFromHost;
 
     @Override
     public void onEnable() {
+        int ID = 16236;
+        Metrics metrics = new Metrics(this, ID);
+
         BungeePlayerFromHost = this;
         getProxy().getPluginManager().registerListener(this, new Event());
         getProxy().getPluginManager().registerCommand(this, new Reload());
